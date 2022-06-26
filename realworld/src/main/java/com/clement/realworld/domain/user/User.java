833b2e99d41ed5b320d2404680e003d5b4983ff5
phyauab <phyauab@connect.ust.hk>
2022-06-26 13:25:32 +0800

    Initial commit

---
+++ realworld/src/main/java/com/clement/realworld/domain/user/User.java
@@ -0,0 +1,7 @@
+package com.clement.realworld.domain.user;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+}
---
+++ realworld/src/main/java/com/clement/realworld/domain/user/role/Role.java
@@ -0,0 +1,24 @@
+package com.clement.realworld.domain.user.role;
+
+import com.clement.realworld.domain.user.role.ERole;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+
+}
