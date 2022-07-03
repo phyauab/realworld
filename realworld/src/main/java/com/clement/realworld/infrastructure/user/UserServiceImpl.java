5177dabd97bb02b7ff22e2b8b80f33b854de783a
phyauab <phyauab@connect.ust.hk>
2022-07-03 12:05:42 +0800

    clean up

--- realworld/src/main/java/com/clement/realworld/infrastructure/user/UserServiceImpl.java
+++ realworld/src/main/java/com/clement/realworld/infrastructure/user/UserServiceImpl.java
@@ -15,18 +15,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
