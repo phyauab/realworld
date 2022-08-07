dc44d48a516aa6d9ab15d4d552edc8f2de98c7bb
phyauab <phyauab@connect.ust.hk>
2022-08-07 20:08:31 +0800

    update validation error response

--- realworld/src/main/java/com/clement/realworld/application/exception/GlobalExceptionHandler.java
+++ realworld/src/main/java/com/clement/realworld/application/exception/GlobalExceptionHandler.java
@@ -2,11 +2,22 @@ package com.clement.realworld.application.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        ValidationErrorResponse response = new ValidationErrorResponse();
+        for(FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            response.append(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return new ResponseEntity<>(response, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<GenericErrorResponse> handleException(Exception e) {
--- realworld/src/main/java/com/clement/realworld/application/exception/ValidationErrorResponse.java
+++ realworld/src/main/java/com/clement/realworld/application/exception/ValidationErrorResponse.java
@@ -4,20 +4,33 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.WRAPPER_OBJECT;
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
 
 @Getter
-@JsonTypeName(value = "errors")
-@JsonTypeInfo(use = NAME, include = WRAPPER_OBJECT)
 public class ValidationErrorResponse {
 
-    private List<String> body;
+    private Map<String, ArrayList<String>> errors;
 
-    private void append(String message) {
-        this.body.add(message);
+    public ValidationErrorResponse() {
+        errors = new HashMap<>();
+    }
+
+    public void append(String key, String message) {
+        ArrayList<String> arrayList = null;
+        if(errors.get(key) == null) {
+            arrayList = new ArrayList<>();
+            arrayList.add(message);
+            this.errors.put(key, arrayList);
+        } else {
+            arrayList = this.errors.get(key);
+            arrayList.add(message);
+        }
     }
 
 }
